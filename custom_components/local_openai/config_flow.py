skye-harris/hass_local_openai_llm

commit f560856659f9d65cf0dcc97ecd66dda8e20a7106

Replace enable_thinking with a more generic chat_template_kwargs key/value setup - Value fields accept jinja templates, in order to provide non-string data type support - Updated readme Bumped minor

custom_components/local_openai/config_flow.py
@@ -15,28 +15,32 @@ from homeassistant.config_entries import (
 )
 from homeassistant.const import CONF_API_KEY, CONF_LLM_HASS_API, CONF_MODEL, CONF_PROMPT
 from homeassistant.core import HomeAssistant, callback
-from homeassistant.data_entry_flow import section
+from homeassistant.data_entry_flow import SectionConfig, section
 from homeassistant.helpers import llm
 from homeassistant.helpers.httpx_client import get_async_client
 from homeassistant.helpers.selector import (
     NumberSelector,
     NumberSelectorConfig,
     NumberSelectorMode,
+    ObjectSelector,
     SelectOptionDict,
     SelectSelector,
     SelectSelectorConfig,
-    TemplateSelector,
     SelectSelectorMode,
+    TemplateSelector,
 )
 from openai import AsyncOpenAI, OpenAIError
 
 from .const import (
     CONF_BASE_URL,
+    CONF_CHAT_TEMPLATE_KWARGS,
+    CONF_CHAT_TEMPLATE_OPTS,
+    CONF_CONTENT_INJECTION_METHOD,
+    CONF_CONTENT_INJECTION_METHODS,
     CONF_MAX_MESSAGE_HISTORY,
     CONF_PARALLEL_TOOL_CALLS,
     CONF_SERVER_NAME,
     CONF_STRIP_EMOJIS,
-    CONF_ENABLE_THINKING,
     CONF_TEMPERATURE,
     CONF_WEAVIATE_API_KEY,
     CONF_WEAVIATE_CLASS_NAME,
@@ -53,8 +57,6 @@ from .const import (
     DOMAIN,
     LOGGER,
     RECOMMENDED_CONVERSATION_OPTIONS,
-    CONF_CONTENT_INJECTION_METHODS,
-    CONF_CONTENT_INJECTION_METHOD,
 )
 from .weaviate import WeaviateClient, WeaviateError
 
@@ -274,69 +276,81 @@ class ConversationFlowHandler(LocalAiSubentryFlowHandler):
             LOGGER.exception(f"Unexpected exception retrieving models list: {err}")
             downloaded_models = []
 
-        schema = {
-            vol.Required(
-                CONF_MODEL,
-            ): SelectSelector(
-                SelectSelectorConfig(options=downloaded_models, custom_value=True)
-            ),
-            vol.Optional(
-                CONF_PROMPT,
-                default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_PROMPT],
-            ): TemplateSelector(),
-            vol.Optional(
-                CONF_LLM_HASS_API,
-                default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_LLM_HASS_API],
-            ): SelectSelector(SelectSelectorConfig(options=llm_apis, multiple=True)),
-            vol.Required(
-                CONF_PARALLEL_TOOL_CALLS,
-                default=True,
-            ): bool,
-            vol.Required(
-                CONF_STRIP_EMOJIS,
-                default=False,
-            ): bool,
-            vol.Optional(
-                CONF_ENABLE_THINKING,
-                default=False,
-            ): SelectSelector(
-                SelectSelectorConfig(
-                    mode=SelectSelectorMode.DROPDOWN,
-                    options=[
-                        SelectOptionDict(value="true", label="On"),
-                        SelectOptionDict(value="false", label="Off")
-                    ],
-                )
-            ),
-            vol.Required(
-                CONF_TEMPERATURE,
-                default=0.6,
-            ): NumberSelector(
-                NumberSelectorConfig(
-                    min=0, max=1, step=0.01, mode=NumberSelectorMode.BOX
-                )
-            ),
-            vol.Optional(
-                CONF_MAX_MESSAGE_HISTORY,
-                default=0,
-            ): NumberSelector(
-                NumberSelectorConfig(
-                    min=0,
-                    max=50,
-                    step=1,
-                    mode=NumberSelectorMode.BOX,
-                )
-            ),
-            vol.Optional(
-                CONF_CONTENT_INJECTION_METHOD,
-            ): SelectSelector(
-                SelectSelectorConfig(
-                    mode=SelectSelectorMode.DROPDOWN,
-                    options=CONF_CONTENT_INJECTION_METHODS,
-                )
-            ),
-        }
         if entry.data.get(CONF_WEAVIATE_OPTIONS, {}).get(CONF_WEAVIATE_HOST):
+            schema = {
+                vol.Required(
+                    CONF_MODEL,
+                ): SelectSelector(
+                    SelectSelectorConfig(options=downloaded_models, custom_value=True)
+                ),
+                vol.Optional(
+                    CONF_PROMPT,
+                    default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_PROMPT],
+                ): TemplateSelector(),
+                vol.Optional(
+                    CONF_LLM_HASS_API,
+                    default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_LLM_HASS_API],
+                ): SelectSelector(
+                    SelectSelectorConfig(options=llm_apis, multiple=True)
+                ),
+                vol.Required(
+                    CONF_PARALLEL_TOOL_CALLS,
+                    default=True,
+                ): bool,
+                vol.Required(
+                    CONF_STRIP_EMOJIS,
+                    default=False,
+                ): bool,
+                vol.Required(
+                    CONF_TEMPERATURE,
+                    default=0.6,
+                ): NumberSelector(
+                    NumberSelectorConfig(
+                        min=0, max=1, step=0.01, mode=NumberSelectorMode.BOX
+                    )
+                ),
+                vol.Optional(
+                    CONF_MAX_MESSAGE_HISTORY,
+                    default=0,
+                ): NumberSelector(
+                    NumberSelectorConfig(
+                        min=0,
+                        max=50,
+                        step=1,
+                        mode=NumberSelectorMode.BOX,
+                    )
+                ),
+                vol.Optional(
+                    CONF_CONTENT_INJECTION_METHOD,
+                ): SelectSelector(
+                    SelectSelectorConfig(
+                        mode=SelectSelectorMode.DROPDOWN,
+                        options=CONF_CONTENT_INJECTION_METHODS,
+                    )
+                ),
+                vol.Required(CONF_CHAT_TEMPLATE_OPTS): section(
+                    options=SectionConfig(collapsed=True),
+                    schema=vol.Schema(
+                        schema={
+                            vol.Required(CONF_CHAT_TEMPLATE_KWARGS): ObjectSelector(
+                                config={
+                                    "multiple": True,
+                                    "fields": {
+                                        "Name": {
+                                            "selector": {"text": None},
+                                            "required": True,
+                                        },
+                                        "Value": {
+                                            "selector": {"template": None},
+                                            "required": True,
+                                        },
+                                    },
+                                }
+                            ),
+                        }
+                    ),
+                ),
+            }
             schema = {
                 **schema,
                 vol.Optional(CONF_WEAVIATE_OPTIONS): section(
@@ -381,7 +395,7 @@ class ConversationFlowHandler(LocalAiSubentryFlowHandler):
                             ),
                         }
                     ),
-                    options={"collapsed": True},
+                    options=SectionConfig(collapsed=True),
                 ),
             }
 

custom_components/local_openai/const.py
@@ -15,7 +15,8 @@ CONF_STRIP_EMOJIS = "strip_emojis"
 CONF_MAX_MESSAGE_HISTORY = "max_message_history"
 CONF_TEMPERATURE = "temperature"
 CONF_PARALLEL_TOOL_CALLS = "parallel_tool_calls"
-CONF_ENABLE_THINKING = "enable_thinking"
+CONF_CHAT_TEMPLATE_OPTS = "chat_template_opts"
+CONF_CHAT_TEMPLATE_KWARGS = "chat_template_kwargs"
 
 CONF_CONTENT_INJECTION_METHOD_SYSTEM = "System"
 CONF_CONTENT_INJECTION_METHOD_ASSISTANT = "Assistant"

custom_components/local_openai/entity.py
@@ -7,8 +7,8 @@ import base64
 import json
 import uuid
 from collections.abc import AsyncGenerator, Callable
-from typing import TYPE_CHECKING, Any, Literal
 from datetime import datetime
+from typing import TYPE_CHECKING, Any, Literal
 
 import demoji
 import openai
@@ -18,7 +18,7 @@ from homeassistant.config_entries import ConfigSubentry
 from homeassistant.const import CONF_MODEL
 from homeassistant.exceptions import HomeAssistantError
 from homeassistant.helpers import device_registry as dr
-from homeassistant.helpers import llm
+from homeassistant.helpers import llm, template
 from homeassistant.helpers.entity import Entity
 from openai._streaming import AsyncStream
 from openai.types.chat import (
@@ -40,10 +40,15 @@ from voluptuous_openapi import convert
 
 from . import LocalAiConfigEntry
 from .const import (
+    CONF_CHAT_TEMPLATE_KWARGS,
+    CONF_CHAT_TEMPLATE_OPTS,
+    CONF_CONTENT_INJECTION_METHOD,
+    CONF_CONTENT_INJECTION_METHOD_ASSISTANT,
+    CONF_CONTENT_INJECTION_METHOD_TOOL,
+    CONF_CONTENT_INJECTION_METHOD_USER,
     CONF_MAX_MESSAGE_HISTORY,
     CONF_PARALLEL_TOOL_CALLS,
     CONF_STRIP_EMOJIS,
-    CONF_ENABLE_THINKING,
     CONF_TEMPERATURE,
     CONF_WEAVIATE_API_KEY,
     CONF_WEAVIATE_CLASS_NAME,
@@ -58,10 +63,6 @@ from .const import (
     CONF_WEAVIATE_THRESHOLD,
     DOMAIN,
     LOGGER,
-    CONF_CONTENT_INJECTION_METHOD,
-    CONF_CONTENT_INJECTION_METHOD_ASSISTANT,
-    CONF_CONTENT_INJECTION_METHOD_USER,
-    CONF_CONTENT_INJECTION_METHOD_TOOL,
 )
 from .weaviate import WeaviateClient
 
@@ -72,8 +73,7 @@ MAX_TOOL_ITERATIONS = 10
 def _remove_unsupported_keys_from_tool_schema(schema: dict[str, Any]) -> None:
     """Remove keys not supported in the tool schema"""
     for key in ("allOf", "anyOf", "oneOf"):
-        if key in schema:
-            del schema[key]
+        schema.pop(key, None)
 
 
 def _adjust_schema(schema: dict[str, Any]) -> None:
@@ -150,8 +150,11 @@ async def _convert_content_to_chat_message(
 ) -> ChatCompletionMessageParam | None:
     """Convert any native chat message for this agent to the native format."""
     if isinstance(content, conversation.ToolResultContent):
+
         def log_and_str(value) -> str:
-            LOGGER.warning(f"Attempting string convertion of non-JSON-serialisable response content from LLM tool '{content.tool_name}': {value}")
+            LOGGER.warning(
+                f"Attempting string convertion of non-JSON-serialisable response content from LLM tool '{content.tool_name}': {value}"
+            )
             return str(value)
 
         return ChatCompletionToolMessageParam(
@@ -374,7 +377,6 @@ class LocalAiEntity(Entity):
         max_message_history = options.get(CONF_MAX_MESSAGE_HISTORY, 0)
         temperature = options.get(CONF_TEMPERATURE, 0.6)
         parallel_tool_calls = options.get(CONF_PARALLEL_TOOL_CALLS, True)
-        enable_thinking = options.get(CONF_ENABLE_THINKING)
 
         model_args = {
             "model": self.model,
@@ -477,21 +479,31 @@ class LocalAiEntity(Entity):
                     for tool in tools
                     if not tool["function"]["name"].endswith("GetDateTime")
                 ]
+        model_args["messages"] = messages
 
         if tools:
             model_args["tools"] = tools
 
-        model_args["messages"] = messages
+        chat_template_opts = options.get(CONF_CHAT_TEMPLATE_OPTS, {})
+        chat_template_args = chat_template_opts.get(CONF_CHAT_TEMPLATE_KWARGS, [])
 
-        # this is a string (config thing from voluptuous), so a truey check is fine
-        if enable_thinking:
-            thinking_val = True if enable_thinking == "true" else False
-            LOGGER.debug(f"Adding `chat_template_kwargs` to payload with `enable_thinking` set to {enable_thinking}")
-            model_args["extra_body"] = {
-                "chat_template_kwargs": {
-                    "enable_thinking": thinking_val
-                }
-            }
+        # Filter args without a name - they are marked as required in the schema but this isn't being enforced on the front-end
+        chat_template_args = [
+            keypair for keypair in chat_template_args if keypair["Name"].strip()
+        ]
+
+        if chat_template_args:
+            kwargs = {}
+            for keypair in chat_template_args:
+                if keypair["Name"]:
+                    # Our value is a template, so that non-string data types and more complex structures can be provided by the user
+                    kwargs[keypair["Name"]] = template.Template(
+                        keypair["Value"],
+                        self.hass,
+                    ).async_render()
+
+            LOGGER.debug(f"Chat template kwargs: {kwargs}")
+            model_args["extra_body"] = {"chat_template_kwargs": kwargs}
 
         if structure:
             if TYPE_CHECKING:

commit cf2724111fd052762f5dcd91a63d09959648ae33

config_flow: properly initialize config schema
The schema initialization was erroneously moved under the WEAVIATE
option check. This way, if Weaviate is not active, the schema is
left uninitialized and the routine just crashed when attempting to acces
it upon return.

Revert this little change back.

Fixes: f560856 ("Replace enable_thinking with a more generic chat_template_kwargs key/value setup")
GitHub: fixes skye-harris/hass_local_openai_llm#29
Signed-off-by: Antonio Quartulli <antonio@mandelbit.com>

custom_components/local_openai/config_flow.py
@@ -276,83 +276,84 @@ class ConversationFlowHandler(LocalAiSubentryFlowHandler):
             LOGGER.exception(f"Unexpected exception retrieving models list: {err}")
             downloaded_models = []
 
-        if entry.data.get(CONF_WEAVIATE_OPTIONS, {}).get(CONF_WEAVIATE_HOST):
-            schema = {
-                vol.Required(
-                    CONF_MODEL,
-                ): SelectSelector(
-                    SelectSelectorConfig(options=downloaded_models, custom_value=True)
-                ),
-                vol.Optional(
-                    CONF_PROMPT,
-                    default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_PROMPT],
-                ): TemplateSelector(),
-                vol.Optional(
-                    CONF_LLM_HASS_API,
-                    default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_LLM_HASS_API],
-                ): SelectSelector(
-                    SelectSelectorConfig(options=llm_apis, multiple=True)
-                ),
-                vol.Required(
-                    CONF_PARALLEL_TOOL_CALLS,
-                    default=True,
-                ): bool,
-                vol.Required(
-                    CONF_STRIP_EMOJIS,
-                    default=False,
-                ): bool,
-                vol.Required(
-                    CONF_TEMPERATURE,
-                    default=0.6,
-                ): NumberSelector(
-                    NumberSelectorConfig(
-                        min=0, max=1, step=0.01, mode=NumberSelectorMode.BOX
-                    )
-                ),
-                vol.Optional(
-                    CONF_MAX_MESSAGE_HISTORY,
-                    default=0,
-                ): NumberSelector(
-                    NumberSelectorConfig(
-                        min=0,
-                        max=50,
-                        step=1,
-                        mode=NumberSelectorMode.BOX,
-                    )
-                ),
-                vol.Optional(
-                    CONF_CONTENT_INJECTION_METHOD,
-                ): SelectSelector(
-                    SelectSelectorConfig(
-                        mode=SelectSelectorMode.DROPDOWN,
-                        options=CONF_CONTENT_INJECTION_METHODS,
-                    )
-                ),
-                vol.Required(CONF_CHAT_TEMPLATE_OPTS): section(
-                    options=SectionConfig(collapsed=True),
-                    schema=vol.Schema(
-                        schema={
-                            vol.Required(
-                                CONF_CHAT_TEMPLATE_KWARGS, default=[]
-                            ): ObjectSelector(
-                                config={
-                                    "multiple": True,
-                                    "fields": {
-                                        "Name": {
-                                            "selector": {"text": None},
-                                            "required": True,
-                                        },
-                                        "Value": {
-                                            "selector": {"template": None},
-                                            "required": True,
-                                        },
+        schema = {
+            vol.Required(
+                CONF_MODEL,
+            ): SelectSelector(
+                SelectSelectorConfig(options=downloaded_models, custom_value=True)
+            ),
+            vol.Optional(
+                CONF_PROMPT,
+                default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_PROMPT],
+            ): TemplateSelector(),
+            vol.Optional(
+                CONF_LLM_HASS_API,
+                default=RECOMMENDED_CONVERSATION_OPTIONS[CONF_LLM_HASS_API],
+            ): SelectSelector(
+                SelectSelectorConfig(options=llm_apis, multiple=True)
+            ),
+            vol.Required(
+                CONF_PARALLEL_TOOL_CALLS,
+                default=True,
+            ): bool,
+            vol.Required(
+                CONF_STRIP_EMOJIS,
+                default=False,
+            ): bool,
+            vol.Required(
+                CONF_TEMPERATURE,
+                default=0.6,
+            ): NumberSelector(
+                NumberSelectorConfig(
+                    min=0, max=1, step=0.01, mode=NumberSelectorMode.BOX
+                )
+            ),
+            vol.Optional(
+                CONF_MAX_MESSAGE_HISTORY,
+                default=0,
+            ): NumberSelector(
+                NumberSelectorConfig(
+                    min=0,
+                    max=50,
+                    step=1,
+                    mode=NumberSelectorMode.BOX,
+                )
+            ),
+            vol.Optional(
+                CONF_CONTENT_INJECTION_METHOD,
+            ): SelectSelector(
+                SelectSelectorConfig(
+                    mode=SelectSelectorMode.DROPDOWN,
+                    options=CONF_CONTENT_INJECTION_METHODS,
+                )
+            ),
+            vol.Required(CONF_CHAT_TEMPLATE_OPTS): section(
+                options=SectionConfig(collapsed=True),
+                schema=vol.Schema(
+                    schema={
+                        vol.Required(
+                            CONF_CHAT_TEMPLATE_KWARGS, default=[]
+                        ): ObjectSelector(
+                            config={
+                                "multiple": True,
+                                "fields": {
+                                    "Name": {
+                                        "selector": {"text": None},
+                                        "required": True,
                                     },
-                                }
-                            ),
-                        }
-                    ),
+                                    "Value": {
+                                        "selector": {"template": None},
+                                        "required": True,
+                                    },
+                                },
+                            }
+                        ),
+                    }
                 ),
-            }
+            ),
+        }
+
+        if entry.data.get(CONF_WEAVIATE_OPTIONS, {}).get(CONF_WEAVIATE_HOST):
             schema = {
                 **schema,
                 vol.Optional(CONF_WEAVIATE_OPTIONS): section(